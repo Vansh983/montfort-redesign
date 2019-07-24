3445be3114707b64b1f52b67ac96fd869c79ab53
Vansh Sood <emailofvansh@gmail.com>
2019-07-25 00:44:26 +0530

    chatroom done

--- chat.js
+++ chat.js
@@ -12,29 +12,39 @@ $(function(){
 			$('input').val('');
 			if(i==0){
 			setTimeout(function(){
-			$('.message-area').append('<div class="user-message"><p>'+value+'</p></div>')
+			$('.message-area').append('<div class="user-message">'+value+'</div>')
 		},400)
 			setTimeout(function(){
-			$('.message-area').append('<div class="message"><p>Okay,'+value+', and?</p></div>')
+			if (value == 'yes') {
+				$('.message-area').append('<div class="message">What is your name?</div>')
+			}else if (value == 'no') {
+				$('.message-area').append('<div class="message">No problem, just give me any random name, fake or real.</div>')
+			}else{
+				$('.message-area').append('<div class="message">Answer in yes or no please.</div>')
+				i=0;
+			}
 		},900)
 			}
 			else if(i==1){
 				setTimeout(function(){
-			$('.message-area').append('<div class="user-message"><p>'+value+'</p></div>')
+			$('.message-area').append('<div class="user-message">'+value+'</div>')
 		},400)
 			setTimeout(function(){
-			$('.message-area').append('<div class="message"><p>Would you like to speak to a councellor?</p></div>')
+			$('.message-area').append('<div class="message">Okay '+value+', Would you like to speak to a teacher or a councellor?</div>')
 		},900)
 			}
 			else if(i==2){
 				setTimeout(function(){
-			$('.message-area').append('<div class="user-message"><p>'+value+'</p></div>')
+			$('.message-area').append('<div class="user-message">'+value+'</div>')
 		},400)
 			setTimeout(function(){
 				if (value == 'yes') {
-					$('.message-area').append('<div class="message"><p>You said yes</p></div>')
+					$('.message-area').append('<div class="message">I will try to connect you</div>')
 				}else if (value == 'no') {
-					$('.message-area').append('<div class="message"><p>You said no</p></div>')
+					$('.message-area').append('<div class="message">No? Ok it is your call. You can just speak anything here. It will never be disclosed to anyone I promise. So, Where did the problem begin?</div>')
+				}else{
+					$('.message-area').append('<div class="message">Answer in yes or no please.</div>')
+					i=2;
 				}
 		},900)
 		}
@@ -44,18 +54,12 @@ $(function(){
 			$('.message-area').append('<div class="user-message">'+value+'</div>')
 				}, 300)
 				setTimeout(function(){
-					if(value=='yes' || value=='Yes' || value=='YES'){
-						$('.message-area').append('<div class="message"></div>')
-						$('.message-area').append('<div class="message"></div>')
-							page();
-					}
-					else if(value=='no' || value=='No' || value=='NO'){
-						$('.message-area').append('<div class="message"></div>')
-						$('.message-area').append('<div class="message"></div>')
-					}
-					else{
-						$('.message-area').append('<div class="message"></div>')
-						i=2;
+					if (value=='thats all') {
+						$('.message-area').append('<div class="message">All right! I hope you felt good sharing this with me. And again, this will always stay between you and me ;)</div>');
+
+					}else{
+						$('.message-area').append('<div class="message">Okay...</div>');
+						i=3;
 					}
 				},1000)
 			}
