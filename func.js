0ac22c8a5a24bbf065035d6f927b56b68efffd74
Vansh Sood <emailofvansh@gmail.com>
2019-07-24 20:52:49 +0530

    Landing section plus nav

---
+++ func.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  setInterval(function(){
+    $('.landtext, .line').addClass('active')
+  }, 600)
+  setInterval(function(){
+    $('video').play()
+  }, 0)
+  $('.bars').click(function () {
+    $('.bar1').toggleClass('bar1_ac')
+    $('.bar2').toggleClass('bar2_ac')
+    $('.bar3').toggleClass('bar3_ac')
+    $('.profile').toggleClass('menu_ac')
+  })
+
+})
